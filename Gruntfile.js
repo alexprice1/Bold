c20ecf4cd7ba6684e9101a84752bcf79dbe3f945
Colin Hemphill <colin@hemphill.us>
2014-08-20 11:49:29 -0500

    Setting up grunt and mocha tests.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/* Grunt Task File */
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    simplemocha: {
+      options: {
+        timeout: 3000,
+        ignoreLeaks: false
+      },
+      all: {
+        src: ['example/test.js']
+      }
+    }
+  });
+
+
+  // load tasks
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  // register tasks
+  grunt.registerTask('test', 'simplemocha');
+};
---
+++ example/test.js
@@ -0,0 +1,20 @@
+/* Requirements */
+var expect = require('chai').expect;
+var clc = require('cli-color');
+
+var error = clc.red;
+
+describe('server test', function() {
+
+  // server setup
+  before(function(done) {
+    done();
+  });
+
+  // test server
+  it('Tests to ensure server setup is configured correctly', function() {
+    var test = true;
+    expect(result).equals(true);
+  });
+
+});
