38d7a8a9bd85b37a401020313360d1803ee70e3d
Colin Hemphill <colin@hemphill.us>
2015-01-09 17:16:43 -0600

    Fix Q implementation.

--- example/api/afterware.js
+++ example/api/afterware.js
@@ -2,6 +2,6 @@ exports.testLog = function(err, res, data, session, extras) {
   if (err) {
     return console.log('The API experienced an error. Log the error to the DB.')
   } else {
-    return console.log('We can log the number ' + res + 'to the DB.');
+    return console.log('We can log the number ' + res + ' to the DB.');
   }
 };
